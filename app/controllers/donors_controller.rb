98f15c4fd67a331b5dabcc54ce673031b8499b75
Pedro Resende <pedroresende@MacBook-Pro-de-Pedro.local.gap.com>
2016-07-26 16:46:31 -0300

    updated .gitignore

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -2,14 +2,21 @@ class DonorsController < ApplicationController
 
 	def create
 		p donor_params
-		donor = Donor.new(donor_params)
+		donor = Donor.create!(donor_params)
 		respond_to do |format|
 		  format.json { render :json => donor }
 	    end
 	end
 
+	def show
+		@donor = Donor.find(params[:id])
+		respond_to do |format|
+		  format.json { render :json => @donor }
+	    end
+	end
+
 	private 
 	def donor_params
-		params.require(:donor).permit(:name, :cpf, :email, :birth_date, :blood_type, :sex, :contact, :health_info, {:address => [:city]})
+		params.require(:donor).permit(:name, :cpf, :email, :birth_date, :blood_type, :sex, :contact, :health_info, {:address => [:city, :street, :number, :state, :country]})
 	end
 end
